24cb91b8e865ca4749417139848c4c6bf43a1981
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-02-06 12:23:41 -0500

    meets expectations, needs refactoring for event bubbling when pagination buttons are clicked

--- main.js
+++ main.js
@@ -13,37 +13,6 @@
 // 
 // The project also makes use of jQuery.
 
-// ---------------------
-// Pseudocode to meet expectations
-
-// at first page load, display only the first ten students
-//  pass students to an array 
-//  call function hideStudents() at paginationLinkIndex 1 to hide students from 11 to the end of the array (array.length-1)
-
-// function hideStudents(paginationLinkIndex, studentArray):
-//  determine start of index in array:
-//      start of index = (pagination link index * 10) - 10)
-//      end of index = (pagination link index * 10) - 1) OR if this is the last page, array.length-1
-//  check if pagination link index is at 0 (thus at the start of the list); if not, hide
-//  all student-items from index 0 to start of index - 1 (thus just before the list to display)
-//  show all student-items from start of index to end of index
-//  check if this is the last page by checking whether there is a student-item following end of index (thus end of index + 1)
-//  if this is not the last page, hide all student-items after the index to end of array
-
-// function to dynamically create pagination HTML to display buttons
-//  (takes an argument pageLinkIndex to determine active page link index)
-//  (takes an argument array length to determine how many buttons is needed)
-//  if there is an old div with class pagination, remove this from the page 
-//  use a variable htmlString to build up HTML to add to DOM
-//  create a div with class="pagination" and a <ul> element
-//  cycle through (length of array / 10) times adding <li> elements AND if there is a remainder (use %) one extra time
-//  close </ul> and div
-//  append htmlString to correct place in DOM
-//  add a class of "active" to the button of the nth index in the pagination buttons
-
-// if link is clicked, reload students, this time with new multiple of 10
-// (e.g. if link 2 is clicked, display students 11-20)
-
 // make an array of all the students in the list
 const studentsArray = document.getElementsByClassName('student-item');
 
@@ -63,7 +32,8 @@ function hideStudents(paginationLinkIndex, array) {
         }
     }
 
-    for (let i = indexStartPos; i >= indexEndPos; i++) {
+    // show the students that are in scope
+    for (let i = indexStartPos; i <= indexEndPos; i++) {
         $(array[i]).show();
     }
 
@@ -72,14 +42,15 @@ function hideStudents(paginationLinkIndex, array) {
         for (let i = indexEndPos + 1; i < array.length; i++) {
             $(array[i]).hide();
         }
-        console.log('running');
     }
 }
 
+// create the pagination buttons and assign event handlers to them
 function createPaginationButtons(paginationLinkIndex, arrayLength) {
     // if there is an old div with class pagination, remove this from the page 
     if ($('.pagination')) { $('.pagination').remove(); }
 
+    // build an HTML string to add to the DOM with the pagination markup
     let htmlString = '<div class="pagination"><ul>';
     let numButtons = (arrayLength / 10);
     if (arrayLength % 10 > 0) numButtons++; // to account for the final page, if there are more students than a multiple of ten 
@@ -90,20 +61,23 @@ function createPaginationButtons(paginationLinkIndex, arrayLength) {
     }
     htmlString += '</ul></div>';
 
-    // append to the page
+    // append HTML string to the page
     $('.page').append(htmlString);
-}
 
-// function to dynamically create pagination HTML to display buttons
-//  (takes an argument pageLinkIndex to determine active page link index)
-//  (takes an argument array length to determine how many buttons is needed)
-//  if there is an old div with class pagination, remove this from the page 
-//  use a variable htmlString to build up HTML to add to DOM
-//  create a div with class="pagination" and a <ul> element
-//  cycle through (length of array / 10) times AND if there is a remainder (use %) one extra time
-//  close </ul> and div
-//  append htmlString to correct place in DOM
-//  add a class of "active" to the button of the nth index in the pagination buttons
+    // add event handlers to all the buttons
+    // first, select all the newly created buttons
+    let $paginationButtons = $('.pagination li');
+    // add the event handlers to each button in the list 
+    for (let i = 0; i <= numButtons - 1; i++) {
+        $($paginationButtons[i]).on('click', (event) => {
+
+            // determine number of button pressed
+            let buttonPressed = $($paginationButtons[i]).index() + 1;
+            hideStudents(buttonPressed, studentsArray);
+            createPaginationButtons(buttonPressed, studentsArray.length);
+        });
+    }
+}
 
 // at first page load, display only the first ten students
 hideStudents(1, studentsArray);
